3765d8c89cf55035b694b53099cfa847c1842675
Theo-encipher <83564917+Theo-encipher@users.noreply.github.com>
2022-05-20 11:31:58 +0930

    got add postion working except for add end

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -17,7 +17,7 @@ LinkedList::LinkedList(int * array, int size) {
     //make the head a node
     head = new Node();
 
-    //dyamically create a new node
+    //dynamically create a new node
     Node * obj1 = new Node();
 
     //make head point to the new Node
@@ -60,11 +60,67 @@ void LinkedList::addFront(int newItem) {
 }
 
 void LinkedList::addEnd(int newItem) {
+    //pointer to move through list
+
+    Node * pointer = head->getNext();
+
+    //move through until final node is reached
+    while (pointer != nullptr) {
+        pointer = pointer->getNext();
+    }
+
+    //create new node to add
+    Node * obj = new Node();
+    //set new data to new node
+    obj->setData(newItem);
+
+    //make old end node point to new end node
+    pointer->setNext(obj);
 
 }
 
 void LinkedList::addAtPosition(int position, int newItem) {
-    addFront(newItem);
+    //variable to find position
+    int count = 1;
+
+    //pointer to move through list
+    Node* pointer = head->getNext();
+
+    //position less than one add at the start of the linkedList
+    if ( position < 1) {
+        addFront(newItem);
+    }
+
+    else {
+        //find the node before the position the new node is being added at or find position is bigger than size of array, hence add at end;
+        for (pointer; pointer != nullptr; pointer = pointer->getNext()) {
+            count++;
+            if (count == (position - 1)) {
+                break;
+            }
+        }
+
+        if (pointer == nullptr) {
+            addEnd(newItem);
+        }
+
+        else {
+            //store pointer before position that new node is added at
+            Node *temp = pointer->getNext();
+
+            //create new node
+            Node *obj = new Node;
+
+            //set new data
+            obj->setData(newItem);
+
+            //make object to node after it
+            obj->setNext(temp);
+
+            //make node before point to new node
+            pointer->setNext(obj);
+        }
+    }
 }
 
 int LinkedList::search(int item) {
@@ -89,7 +145,7 @@ void LinkedList::printItems() {
     //pointer for moving though nodes
     Node * iterator;
 
-    //for loop to iterator through using the pointer //Note can't start at head because that doesn't hold any values;
+    //for loop to iterator through using the pointer //Note can't start at head because that doesn't hold any values
     for (iterator = head->getNext(); iterator != nullptr; iterator = iterator->getNext() ) {
         std::cout << iterator->getData() << " ";
     }
